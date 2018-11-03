82bfcf1f60daeb607edd3bbce29f5c82ca8b1f9a
jarretbryan <jarret.bryan@gmail.com>
2018-11-03 12:28:05 -0400

    build out dept controller

--- app/controllers/api/v1/departments_controller.rb
+++ app/controllers/api/v1/departments_controller.rb
@@ -1,8 +1,39 @@
 class Api::V1::DepartmentsController < ApplicationController
 
+    before_action :get_dept, only: [:show, :destroy]
+
     def index
         @departments = Department.all
         render json: @departments
     end
+
+    def show
+        render json: @department
+    end
+
+    def create
+        @department = Department.create(dept_params)
+        if @department.save
+            render json: @department, status: :accepted
+        else
+            render json: { errors: @department.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+    def destroy
+        @department.delete
+    end
+
+    private
+
+    def get_dept
+        @department = Department.find(params[:id])
+    end
+
+    def dept_params
+        params.permit(:name)
+    end
+
+
 
 end
